96aa00a271c888408fccdea1bb83a6e515004588
Narzo <cricketwithnarzo@gmail.com>
2025-08-15 16:17:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21551509'))
-API_HASH = environ.get('API_HASH', 'cb3d60057fce5975e57b08a50cf57ed3')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7912425927:AAGNp9fISNLXWdcaCYRH8ULRX0tL4cnKQdE')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 # ============================
 # Bot Settings Configuration
 # ============================
