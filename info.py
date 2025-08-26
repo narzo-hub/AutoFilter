6b70011a0bcdb2ac3f5d58f2e666c6608fb62234
Narzo <cricketwithnarzo@gmail.com>
2025-08-26 10:24:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://i.ibb.co/PG9BXWzX/file-293.jpg https://i.ibb.co/FqVVPkR7/file-292.jpg')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://i.ibb.co/FqVVPkR7/file-292.jpg https://i.ibb.co/DPdCwkj4/file-294.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/Wdj.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/Wdj.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/Wdj.jpg")
