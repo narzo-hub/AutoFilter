1c6c056a4ac0f82195f77ad058793b2d7f313656
Narzo <cricketwithnarzo@gmail.com>
2025-09-09 19:36:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,10 @@ PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', 0))  # Premium logs channel id
 auth_channel = environ.get('AUTH_CHANNEL', '-1002558348052')  # Channel/Group ID for force sub (make sure bot is admin)
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '').split()]
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002995638706')  # Support group id (make sure bot is admin)
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002831115957')  # Request channel id (make sure bot is admin)
+# This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
+reqst_channel = environ.get('REQST_CHANNEL', '-1002831115957')
+REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
+
 AUTH_CHANNEL = [int(fch) if id_pattern.search(fch) else fch for fch in environ.get('AUTH_CHANNEL', '').split()]
 MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002576095079').split() if re.match(r'^-?\d+$', channel_id)]  # Channel for force sub (make sure bot is admin)
 
