b4e11cc210734095f3e1fe696acdea5048ec254b
Narzo <cricketwithnarzo@gmail.com>
2025-09-09 18:57:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1087,7 +1087,7 @@ async def requests(bot, message):
         except Exception as e:
             await message.reply_text(f"Error: {e}")
             pass
-     elif message.text:
+     elif SUPPORT_CHAT_ID == message.chat.id:
         chat_id = message.chat.id
         reporter = str(message.from_user.id)
         mention = message.from_user.mention
