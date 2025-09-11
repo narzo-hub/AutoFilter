cbbb03262c109c1b3f3835b10f4672119a519e4b
Narzo <cricketwithnarzo@gmail.com>
2025-09-11 10:01:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1094,8 +1094,7 @@ async def requests(bot, message):
             InlineKeyboardButton('View Request', url=f"{reported_post.link}")
         ]]
         await message.reply_text("<b>Your request has been added! Please wait for some time.\n\nJoin Channel First & View Request</b>", reply_markup=InlineKeyboardMarkup(btn))
-    
-        
+
     
 @Client.on_message(filters.command("send") & filters.user(ADMINS))
 async def send_msg(bot, message):
