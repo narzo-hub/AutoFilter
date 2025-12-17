25663bbcb10cb392772bfcb0f953c749e4057dd4
Narzo <cricketwithnarzo@gmail.com>
2025-12-17 13:08:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ FSUB_PICS = (environ.get('FSUB_PICS', 'https://envs.sh/tKX.jpg')).split()  # Fsu
 # ============================
 # Admin, Channels & Users Configuration
 # ============================
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7123837674').split()] # Replace with the actual admin ID(s) to add
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # Replace with the actual admin ID(s) to add
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003030799577').split()]  # Channel id for auto indexing (make sure bot is admin)
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002881534182'))  # Log channel id (make sure bot is admin)
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002831115957'))  # Bin channel id (make sure bot is admin)
