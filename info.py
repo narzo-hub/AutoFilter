f3a018d471905b33854fc7a79b36ec5a16a4d2df
Narzo <cricketwithnarzo@gmail.com>
2026-01-15 12:08:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,9 +69,9 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Yashds:Yashds@yashds.ttyep08.mongodb.net/?retryWrites=true&w=majority&appName=Yashds")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Yash:yash123@cluster0.ziooass.mongodb.net/?appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Yashds:yashds@yashds.xzuhxvo.mongodb.net/?retryWrites=true&w=majority&appName=Yashds")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Yashds")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Lucy_files')
 
 # ============================
